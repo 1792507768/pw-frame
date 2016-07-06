f0f40fc814ce96979f17b5cfe03fcc879451b214
iisquare <iisquare@163.com>
2016-07-06 11:55:20 +0800

    index.php

--- code/web/index.php
+++ code/web/index.php
@@ -9,7 +9,7 @@ require_once __DIR__.'/../lib/frame/Application.php';
 try {
     $app = new Application('/', __DIR__);
     $result = $app->run();
-    if(null !== $result) throw Exception($result);
+    if(null !== $result) throw Exception(strval($result));
 } catch (Exception $e) {
     if(DEBUG) throw $e;
 }
