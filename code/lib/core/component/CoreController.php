e82ecec2f50d1792bd6469dac28ae060fa961103
iisquare <iisquare@163.com>
2016-07-06 14:51:18 +0800

    rename appUrl to webUrl in template

--- code/lib/core/component/CoreController.php
+++ code/lib/core/component/CoreController.php
@@ -6,7 +6,7 @@ use pwframe\lib\frame\mvc\ControllerBase;
 abstract class CoreController extends ControllerBase {
     
     public function displayTemplate($action = null, $controller = null) {
-        $this->assign('appUrl', $this->appUrl);
+        $this->assign('webUrl', trim($this->appPath, '/'));
         return parent::displayTemplate($action, $controller);
     }
     
